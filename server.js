3b583753b6143fc314169eb93ec2d980cc2993f2
Yang-tae-wook <35125285+Yang-tae-wook@users.noreply.github.com>
2018-01-22 15:07:08 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+
+app.get('/keyboard',function(req,res){ // setting keyboard for first open
+  let keyboard = {
+    "type" : "text"
+    /*
+    or button, like this
+    "type" : "buttons",
+    "buttons" : ["btn 1", "btn 2", "btn 3"]
+    */
+  };
+  res.send(keyboard);
+});
